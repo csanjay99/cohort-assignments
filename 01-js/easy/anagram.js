ac24c9763435bb6cdec20ff4a8af06441eebf316
csanjay99 <csanjay99@hotmail.com>
2024-04-15 22:58:14 +0530

    1st commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length === str2.length) {
+    let temp = str2;
+    for (let i = 0; i < str1.length; i++) {
+      const index = str2.indexOf(str1[i]);
+      if (index !== -1) {
+        temp = str2.replace(temp, str2)
+      }
+    }
+  }
 }
 
 module.exports = isAnagram;
