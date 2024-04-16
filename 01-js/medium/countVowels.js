8ceb70b1bbb6c6353ec5373dab1f721ae4a4f020
csanjay99 <csanjay99@hotmail.com>
2024-04-16 23:51:15 +0530

    solved palindrome and vowels in medium

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,17 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let count = 0;
+  const strArray = str.split('');
+  for (let i = 0; i < strArray.length; i++) {
+    const letter = strArray[i];
+    if (vowels.includes(letter) || vowels.includes(letter.toLowerCase())) {
+      count += 1
+    }
+  }
+  return count
 }
 
 module.exports = countVowels;
+// console.log(countVowels('perhaps'));
--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,30 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function cleanArray(letter) {
+  if (stops.includes(letter)) {
+    return false
+  }
+  return true
+}
+
+let stops = [' ', '.', ',', '"', "'", '?', '/', ';', ':', '!', '(', ')', '{', '}', '[', ']', '\\', '-'];
+
 function isPalindrome(str) {
-  return true;
+  let strArray = str.toLowerCase().split('');
+  let cleanedArray = strArray.filter(cleanArray);
+  let cleanStr = cleanedArray.join('');
+  const revArray = str.toLowerCase().split('').reverse();
+  const cleanRevArray = revArray.filter(cleanArray);
+  const revWord = cleanRevArray.join('');
+
+  if (revWord === cleanStr) {
+    return true
+  }
+
+  return false
 }
 
 module.exports = isPalindrome;
+
+// console.log(isPalindrome('race Car'));
