79cf94e75de9b5d692c3fff9963351abee4c91f0
csanjay99 <csanjay99@hotmail.com>
2024-04-16 23:06:18 +0530

    solved anagram expenditure in easy

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,17 +4,68 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  const categoryList = [];
+  const catOb = {};
+  for (let index = 0; index < transactions.length; index++) {
+    const element = transactions[index];
+    let price = element['price'];
+    let category = element['category'];
+
+    if (category in catOb) {
+      catOb[category] += price;
+    } else {
+      catOb[category] = price;
+    }
+  };
+
+  for (const key in catOb) {
+    categoryList.push({
+      category: key,
+      totalSpent: catOb[key]
+    })
+  }
+  return categoryList
 }
 
 module.exports = calculateTotalSpentByCategory;
+// console.log(calculateTotalSpentByCategory([
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: 'Food',
+//     itemName: 'Pizza',
+//   },
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 30,
+//     category: 'Food',
+//     itemName: 'Pizza',
+//   },
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 5,
+//     category: 'Confectionary',
+//     itemName: 'mandm',
+//   },
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: 'Bevrage',
+//     itemName: 'Milk',
+//   },
+// ]));
