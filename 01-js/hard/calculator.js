7091b556dc01fbd8f55d83c69a3e0dfd811a4157
csanjay99 <csanjay99@hotmail.com>
2024-04-21 20:01:46 +0530

    work in progress

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,102 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = parseFloat('0');
+    this.expression = ''
+  }
 
-module.exports = Calculator;
+  add(num) {
+    this.result += num;
+  }
+
+  sub(num) {
+    this.result -= num;
+  }
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num === 0 || this.result === 0) return "Cannot divide by zero";
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  checkInvalidChar(input) {
+    let regex = /[a-zA-Z]/;
+    let matches = input.match(regex);
+    if (matches !== null) {
+      throw new Error("Invalid non-numerical characters found in the input string.");
+    }
+  }
+  calculate(strInput) {
+    this.checkInvalidChar(strInput)
+    let inputsArray = strInput.split(' ');
+    inputsArray = inputsArray.filter((s) => s !== "");
+    let inputString = inputsArray.join('');
+    let re1 = /\(/gi,
+      result1,
+      openBracketIndices = [];
+    while ((result1 = re1.exec(inputString))) {
+      openBracketIndices.push(result1.index);
+    }
+    console.log(openBracketIndices);
+    let openBracketIndicesReverse = openBracketIndices.reverse();
+    console.log(openBracketIndicesReverse);
+    let re2 = /\)/gi,
+      result2,
+      closeBracketIndices = [];
+    while ((result2 = re2.exec(inputString))) {
+      closeBracketIndices.push(result2.index);
+    }
+    console.log(closeBracketIndices);
+    let prevSubstr;
+    for (let j = 0; j < openBracketIndices.length; j++) {
+      let newSubstr = inputString.slice(openBracketIndices[j], closeBracketIndices[j] + 1)
+      console.log("Substring: " + newSubstr);
+      if (prevSubstr !== '') {
+        newSubstr = newSubstr.replace(prevSubstr, `${this.result}`);
+      }
+      let operandIndex = newSubstr[2];
+      let value;
+      switch (operandIndex) {
+        case '+':
+          value = parseInt(newSubstr[1]) + parseInt(newSubstr[3]);
+          break
+        case '-':
+          value = parseInt(newSubstr[1]) - parseInt(newSubstr[3]);
+          break;
+        case '*':
+          value = parseInt(newSubstr[1]) * parseInt(newSubstr[3]);
+          break;
+        case '/':
+          value = parseInt(newSubstr[1]) / parseInt(newSubstr[3]);
+          break
+        default:
+          throw new Error("Invalid non-numerical characters found in the input string.");
+      }
+      this.result += value;
+      prevSubstr = newSubstr;
+
+    }
+
+    inputString = inputString.replace(inputString.slice(openBracketIndicesReverse[-1], closeBracketIndices[-1]), "");
+    console.log("Input String: " + inputString);
+  }
+
+
+}
+
+// module.exports = Calculator;
+
+c = new Calculator();
+c.calculate("10 + 2 * ( 6 - (4 + 1) / 2 ) + 7");
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -2,12 +2,23 @@
 Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
 Try running it for
 1. Sum from 1-100
-2. Sum from 1-100000
-3. Sum from 1-1000000000
+2. Sum from 1-1,00,000
+3. Sum from 1-1,00,00,00,000
 Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let start = new Date();
+    startTime = start.getTime();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    let end = new Date();
+    endTime = end.getTime();
+    console.log(sum);
+    return endTime - startTime;
 }
+
+console.log(calculateTime(100000000));
